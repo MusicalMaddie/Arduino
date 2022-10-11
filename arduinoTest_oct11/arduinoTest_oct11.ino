bdccd73f3e9ee1819bc7b015b2cb1b9de5ba672c
Maddie Meyers <maddiemeyers@outlook.com>
2022-10-11 13:29:17 -0400

    Create arduinoTest_oct11.ino

---
+++ arduinoTest_oct11/arduinoTest_oct11.ino
@@ -0,0 +1,24 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(13,OUTPUT);
+  // pin 13 is connected to integrated LED on arduinot 
+  // this setup just tells the arduino "Hey, we're going to be...""
+  // working with pin 13, be ready"
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(13,HIGH);
+//(what pin, what do we want to do with it ex: on or off)
+// high means on i guess?up you click the 'send' arrow at the top left of this
+// HIGH = on, LOW = off .... for this test
+delay(500);
+
+//lets make it blink by adding low
+
+digitalWrite(13,LOW);
+delay(500);
+//once set, send and it compiles then loads onto arduino
+
+
+}
